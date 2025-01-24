c84d57edc1926de1745000c9f8f88f882bf5c329
shazon-noname <maryan.20083@gmail.com>
2025-01-24 06:58:44 +0200

    Refactor code // Giving html code from url address

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,19 +2,30 @@ package org.example;
 
 import okhttp3.*;
 
-import java.io.IOException;
+public class App {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Помилка передачі параметрів\nПотрібен один параметр");
+            System.exit(0);
+        }
+        String url = args[0];
+        System.out.println("Url from args  : " + url);
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) throws IOException {
-        Request request = new Request.Builder().url("https://www.google.com/").build();
-        Call call = new OkHttpClient().newCall(request);
-        Response response = call.execute();
-
-        System.out.println(response.body().string());
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                assert response.body() != null;
+                String responseBody = response.body().string();
+                System.out.println("Response : " + responseBody);
+            } else {
+                System.out.println("Request failed with HTTP error code " + response.code());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
